b653c624655eff11fe5d7f3f215b3fe4d4e4f67c
Ashlynn Wood <120209167+ashlynnwood@users.noreply.github.com>
2022-12-22 16:10:47 -0500

    Initialized variables to include certain types of characters and set up conditional statement for password length

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 // Function to prompt user for password options
 function getPasswordOptions() {
   // Variable to store length of password from user input (using prompt)
-  
+  var numberCount = window.prompt("How many characters would you like your password to contain?")
 
   // Conditional statement to check if password length is a number. Prompts end if this evaluates false
   // show alert if the length variable from above isn't a number
-
+  if (numberCount != number) {
+    window.alert('Error: You need to choose a number.');
+  }
   // Conditional statement to check if password length is at least 8 characters long. Prompts end if this evaluates false
   // show alert if the length variable from above is less than 8
-
+  else if (numberCount < 8) {
+    window.alert('Error: Password must be at least 8 characters.');
+  }
   // Conditional statement to check if password length is less than 128 characters long. Prompts end if this evaluates false
   // show alert if the length variable from above is greater than 128
+  else if (numberCount > 128) {
+    window.alert('Error: Password cannot be longer than 128 characters.');
+  getPasswordOptions();
+  }
 
+  
   // Variable to store boolean regarding the inclusion of SPECIAL characters
   var hasSpecialCharacters = confirm(
     'Click OK to confirm including special characters.'
@@ -19,13 +28,19 @@ function getPasswordOptions() {
 
   // Variable to store boolean regarding the inclusion of NUMERIC characters
   // using pattern from above
-
+  var hasNumericCharacters = confirm(
+    'Click OK to confirm including numeric characters.'
+  );
   // Variable to store boolean regarding the inclusion of LOWERCASE characters
   // using pattern from above
-
+  var hasLowerCaseCharacters = confirm(
+    'Click OK to confirm including lowercase characters.'
+  );
   // Variable to store boolean regarding the inclusion of UPPERCASE characters
   // using pattern from above
-
+  var hasUpperCaseCharacters = confirm(
+    'Click OK to confirm including uppercase characters.'
+  );
   // Conditional statement to check if user does not include any types of characters. Password generator ends if all four variables evaluate to false
   // show alert that tells them they need to have at least one type of character
 
