0395a26bd6f8b9ac0980fd757ecd4f296d1f3cf8
Ashlynn Wood <120209167+ashlynnwood@users.noreply.github.com>
2022-12-22 23:13:59 -0500

    Debugged conditional statements for password/numberCount length

--- script.js
+++ script.js
@@ -89,44 +89,46 @@ var upperCaseCharacters = [
 // Function to prompt user for password options
 function getPasswordOptions() {
   // Variable to store length of password from user input (using prompt)
-  var numberCount = window.prompt("How many characters would you like your password to contain?")
+  var numberCount = parseInt(prompt("How many characters would you like your password to contain?", "0"), 10);
+
+  console.log(numberCount);
+  console.log(typeof numberCount);
+  console.log(isNaN(numberCount));
 
   // Conditional statement to check if password length is a number. Prompts end if this evaluates false
   // show alert if the length variable from above isn't a number
-  if (numberCount != number) {
+  if(isNaN(numberCount)) {
     window.alert('Error: You need to choose a number.');
-
-  // Conditional statement to check if password length is at least 8 characters long. Prompts end if this evaluates false
-  // show alert if the length variable from above is less than 8
-  } else if (numberCount < 8) {
-    window.alert('Error: Password must be at least 8 characters.');
+    getPasswordOptions();
   
-  // Conditional statement to check if password length is less than 128 characters long. Prompts end if this evaluates false
-  // show alert if the length variable from above is greater than 128
-  } else if (numberCount > 128) {
+ // Conditional statement to check if password length is at least 8 characters long. Prompts end if this evaluates false
+  // show alert if the length variable from above is less than 8
+  } else if (numberCount < 7) {
+    window.alert('Error: Password must be at least 8 characters.');
+    getPasswordOptions();
+  
+//   // Conditional statement to check if password length is less than 128 characters long. Prompts end if this evaluates false
+//   // show alert if the length variable from above is greater than 128
+  } else if (numberCount > 129) {
     window.alert('Error: Password cannot be longer than 128 characters.');
+    getPasswordOptions();
 
   } else {
- // Variable to store boolean regarding the inclusion of SPECIAL characters
-var hasSpecialCharacters = confirm('Click OK to confirm including special characters.');
+//     // Variable to store boolean regarding the inclusion of SPECIAL characters
+      var hasSpecialCharacters = confirm('Click OK to confirm including special characters.');
+//     // Variable to store boolean regarding the inclusion of NUMERIC characters
+      var hasNumericCharacters = confirm('Click OK to confirm including numeric characters.');
+//     // Variable to store boolean regarding the inclusion of LOWERCASE characters
+      var hasLowerCaseCharacters = confirm('Click OK to confirm including lowercase characters.');
+//     // Variable to store boolean regarding the inclusion of UPPERCASE characters
+      var hasUpperCaseCharacters = confirm('Click OK to confirm including uppercase characters.');
+   };
 
-// Variable to store boolean regarding the inclusion of NUMERIC characters
-// using pattern from above
-var hasNumericCharacters = confirm('Click OK to confirm including numeric characters.');
-// Variable to store boolean regarding the inclusion of LOWERCASE characters
-// using pattern from above
-var hasLowerCaseCharacters = confirm('Click OK to confirm including lowercase characters.');
-// Variable to store boolean regarding the inclusion of UPPERCASE characters
-// using pattern from above
-var hasUpperCaseCharacters = confirm('Click OK to confirm including uppercase characters.');
-};
-
-// Conditional statement to check if user does not include any types of characters. Password generator ends if all four variables evaluate to false
-// show alert that tells them they need to have at least one type of character
+// // Conditional statement to check if user does not include any types of characters. Password generator ends if all four variables evaluate to false
+// // show alert that tells them they need to have at least one type of character
 if (!hasSpecialCharacters && !hasNumericCharacters && !hasLowerCaseCharacters && !hasUpperCaseCharacters) {
   window.alert("You must choose at least one type of character.");
 } 
-
 
 
 // Object to store user input
@@ -141,54 +143,22 @@ var passwordOptions = {
 };
 
 return passwordOptions;
-}
-
-// Function to generate password with user input
-function generatePassword() {
-var options = getPasswordOptions();
-// Variable to store password as it's being concatenated
-var result = [];
-
-// Array to store types of characters to include in password
-var possibleCharacters = [];
-
-// Array to contain one of each type of chosen character to ensure each will be used
-var guaranteedCharacters = [];
-
-// Conditional statement that adds array of special characters into array of possible characters based on user input
-// Push new random special character to guaranteedCharacters
-if (options.hasSpecialCharacters) {
-  // add specialCharacters to the possibleCharacters array
-  // add a random character from specialCharacters to the guaranteedCharacters array
-}
-
-// following the pattern from above, do the same thing for NUMERIC characters
-
-// following the pattern from above, do the same thing for LOWERCASE characters
-
-// following the pattern from above, do the same thing for UPPERCASE characters
+};
+getPasswordOptions();
 
 
-// loop over the options.length, for each iteration - get a random index from the possibleCharacters array and add it to the result variable
 
-// loop over guaranteedCharacters, set result[i] = guaranteedCharacters[i] - in order to include at least one of each guaranteed character
+// // Assignment Code
+// var generateBtn = document.querySelector("#generate");
 
-// change the result into a string
-// return that string
-}
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
+//   passwordText.value = password;
 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// }
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
